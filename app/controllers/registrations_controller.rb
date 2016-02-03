1b11fc76c3c088f83d1854df3ff9b9be2be919f7
robertcramer <robertbcramer@icloud.com>
2016-02-03 10:39:11 -0600

    update registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,35 @@
 class RegistrationsController < ApplicationController
+
+  def create
+    @user = User.new(email: params[:email],
+                      name: params[:name],
+                    password: params[:password])
+    if @user.save
+      render "create.json.jbuilder", status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      render "login.json.jbuilder", status: :ok
+    else
+      render json: { error: "Could not find user for #{params[:email]} or wrong password." },
+             status: :unauthorized
+    end
+  end
+
+  def delete
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      @user.destroy
+      render json: {success: "Delete Success!"}
+    else
+      render json: { error: "Invalid email (#{params[:email]}) or password." },
+             status: :unauthorized
+    end
+  end
 end
