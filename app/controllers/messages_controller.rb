717e5ad014c00033da4819c1a6094340bf3fa098
robertcramer <robertbcramer@icloud.com>
2016-02-08 20:25:27 -0500

    getting twilio to work from app

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,15 +6,18 @@ class MessagesController < ApplicationController
 
     require 'twilio-ruby'
 
+    account_sid = ENV['SID']
+    auth_token = ENV['SECRET']
+
 
 # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new ENV['SID'], ENV['SECRET']
+    @client = Twilio::REST::Client.new account_sid, auth_token
 
-    @client.account.messages.create(
+    @client.account.messages.create({
                                         :from => '+12053156667',
-                                        :to => '+12058375609',
-                                        :body => 'Make sure you eat!',
-                                    )
+                                        :to => '2058375609',
+                                        :body => 'Do not Forget To Eat!!',
+})
     render json: {success: "Your message was sent"}
   end
 end
