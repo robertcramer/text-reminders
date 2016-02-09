f0363bd66489a4a3638d6a948a7320fd2c8f9a57
robertcramer <robertbcramer@icloud.com>
2016-02-08 20:16:37 -0500

    updated messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:create]
+
 
   def create
 
@@ -8,12 +8,13 @@ class MessagesController < ApplicationController
 
 
 # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new ENV["SID"], ENV["SECRET"]
+    @client = Twilio::REST::Client.new ENV['SID'], ENV['SECRET']
 
-    @client.account.messages.create({
+    @client.account.messages.create(
                                         :from => '+12053156667',
                                         :to => '+12058375609',
                                         :body => 'Make sure you eat!',
-                                    })
+                                    )
+    render json: {success: "Your message was sent"}
   end
 end
